f035ee714ff6bdf4fe662198b72578b7e43950c0
Arnold-Mwangi <78974739+Arnold-Mwangi@users.noreply.github.com>
2021-05-20 21:36:59 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿
+namespace kirigwi_player
+{
+    partial class MusicPlayerApp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MusicPlayerApp));
+            this.TopPanel = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lbllogo = new System.Windows.Forms.Label();
+            this.listBoxsongs = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.WindowsMediaPlayer = new AxWMPLib.AxWindowsMediaPlayer();
+            this.lblfooter = new System.Windows.Forms.Label();
+            this.TopPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.WindowsMediaPlayer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // TopPanel
+            // 
+            this.TopPanel.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.TopPanel.Controls.Add(this.pictureBox1);
+            this.TopPanel.Controls.Add(this.lbllogo);
+            this.TopPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.TopPanel.Location = new System.Drawing.Point(0, 0);
+            this.TopPanel.Name = "TopPanel";
+            this.TopPanel.Size = new System.Drawing.Size(800, 20);
+            this.TopPanel.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::kirigwi_player.Properties.Resources.close;
+            this.pictureBox1.Location = new System.Drawing.Point(770, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(30, 29);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // lbllogo
+            // 
+            this.lbllogo.AutoSize = true;
+            this.lbllogo.Font = new System.Drawing.Font("Stencil", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbllogo.Location = new System.Drawing.Point(291, 4);
+            this.lbllogo.Name = "lbllogo";
+            this.lbllogo.Size = new System.Drawing.Size(103, 16);
+            this.lbllogo.TabIndex = 0;
+            this.lbllogo.Text = "Kambi Music";
+            this.lbllogo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lbllogo.Click += new System.EventHandler(this.lbllogo_Click);
+            // 
+            // listBoxsongs
+            // 
+            this.listBoxsongs.BackColor = System.Drawing.Color.Black;
+            this.listBoxsongs.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.listBoxsongs.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBoxsongs.ForeColor = System.Drawing.Color.Gold;
+            this.listBoxsongs.FormattingEnabled = true;
+            this.listBoxsongs.ItemHeight = 16;
+            this.listBoxsongs.Location = new System.Drawing.Point(556, 27);
+            this.listBoxsongs.Name = "listBoxsongs";
+            this.listBoxsongs.Size = new System.Drawing.Size(244, 386);
+            this.listBoxsongs.TabIndex = 1;
+            this.listBoxsongs.SelectedIndexChanged += new System.EventHandler(this.listBoxsongs_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Black;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(635, 421);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(162, 27);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Add songs";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // WindowsMediaPlayer
+            // 
+            this.WindowsMediaPlayer.Enabled = true;
+            this.WindowsMediaPlayer.Location = new System.Drawing.Point(0, 26);
+            this.WindowsMediaPlayer.Name = "WindowsMediaPlayer";
+            this.WindowsMediaPlayer.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("WindowsMediaPlayer.OcxState")));
+            this.WindowsMediaPlayer.Size = new System.Drawing.Size(550, 387);
+            this.WindowsMediaPlayer.TabIndex = 3;
+            // 
+            // lblfooter
+            // 
+            this.lblfooter.AutoSize = true;
+            this.lblfooter.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.lblfooter.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblfooter.Font = new System.Drawing.Font("Monotype Corsiva", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblfooter.ForeColor = System.Drawing.Color.Black;
+            this.lblfooter.Location = new System.Drawing.Point(309, 428);
+            this.lblfooter.Name = "lblfooter";
+            this.lblfooter.Size = new System.Drawing.Size(188, 17);
+            this.lblfooter.TabIndex = 4;
+            this.lblfooter.Text = "developed by Kirigwi Electronics";
+            // 
+            // MusicPlayerApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblfooter);
+            this.Controls.Add(this.WindowsMediaPlayer);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBoxsongs);
+            this.Controls.Add(this.TopPanel);
+            this.ForeColor = System.Drawing.Color.Gold;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MinimumSize = new System.Drawing.Size(100, 100);
+            this.Name = "MusicPlayerApp";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = " ";
+            this.TopPanel.ResumeLayout(false);
+            this.TopPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.WindowsMediaPlayer)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel TopPanel;
+        private System.Windows.Forms.Label lbllogo;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ListBox listBoxsongs;
+        private System.Windows.Forms.Button button1;
+        private AxWMPLib.AxWindowsMediaPlayer WindowsMediaPlayer;
+        private System.Windows.Forms.Label lblfooter;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kirigwi_player
+{
+    public partial class MusicPlayerApp : Form
+    {
+        public MusicPlayerApp()
+        {
+            InitializeComponent();
+        }
+        //create blobal variables of string type array to save name of tracks and path of the track
+        string[] paths, files;
+        private void lbllogo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //code to select songs
+            OpenFileDialog ofd = new OpenFileDialog();
+            //code to select multiple songs
+            ofd.Multiselect = true;
+            if(ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                files = ofd.SafeFileNames;// save the names of the track in files array
+                paths = ofd.FileNames;    // save the paths of the track in paths array
+                //display the music tittle in list box
+                for(int i=0; i<files.Length; i++) {
+                    listBoxsongs.Items.Add(files[i]);// display songs in list box
+                }
+            }
+
+        }
+
+        private void listBoxsongs_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //code to play track
+            WindowsMediaPlayer.URL = paths[listBoxsongs.SelectedIndex];
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            //code to close kambi music
+            this.Close();
+        }
+    }
+}
